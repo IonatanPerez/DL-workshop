8ab22b3c7e364a0bbe5d1ceedb2574b799175630
Ivan Itzcovich <i.itzcovich@gmail.com>
2018-04-17 00:23:01 -0300

    Update model > 97.5%

--- live_mnist.py
+++ live_mnist.py
@@ -25,38 +25,45 @@ def load_trained_model():
 
 def run(sess, x, out):
     cap = cv2.VideoCapture(0)
-    cap.set(3, 512)
-    cap.set(4, 512)
 
     H, W = 28, 28
+    cv2.namedWindow("Frame", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("Frame",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+
+    cv2.namedWindow("Network input", cv2.WINDOW_NORMAL)
 
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
 
+        h, w, c = frame.shape
+
+        # Set squared frame
+        margin = (w - h) // 2
+        frame = frame[:, margin : h + margin, :]
+
         # Our operations on the frame come here
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        img_small = cv2.resize(img, (H, W), interpolation = cv2.INTER_AREA) 
-        img_big = cv2.resize(img, (10*H, 10*W), interpolation = cv2.INTER_AREA) 
+        img = cv2.resize(img, (H, W), interpolation = cv2.INTER_AREA) 
 
-        img_small = 255 - img_small
+        img = 255 - img
 
-        ret, _ = cv2.threshold(img_small, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        ret, _ = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        img_small[img_small <= ret] = 0
+        img[img <= ret] = 0
 
-        img_input = np.reshape(img_small, (1, H, W, 1))
+        img_input = np.reshape(img, (1, H, W, 1))
         img_input = img_input / np.max(img_input)
 
         graph_out, = sess.run([out], feed_dict={x: img_input})
 
         char = np.argmax(np.squeeze(graph_out))
 
-        cv2.putText(img_big, str(char), (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0))
+        cv2.putText(frame, "Detected: {}".format(str(char)), (0, h-10), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0))
 
-        cv2.imshow("Frame", img_big)
-        cv2.imshow("Network input", img_small)
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Network input", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
